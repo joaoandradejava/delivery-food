bb209c542282f05cfda012374e716ac13cde06d7
João Andrade <andradepinheironeto24@hotmail.com>
2023-05-28 01:44:28 -0300

    Primeiro commit

---
+++ src/main/java/com/joaoandrade/deliveryfood/DeliveryFoodApplication.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.deliveryfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoandrade/deliveryfood/DeliveryFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.deliveryfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
